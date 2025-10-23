25b7fc03b612c0c8d4390a04cb07eab7bc6bd752
renesanger <renejsanger@gmail.com>
2025-10-23 16:20:22 -0400

    fixed Custom Bar error

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,20 @@
+import CustomTabs from "@/components/CustomTabs";
 import { Tabs } from "expo-router";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const _layout = () => {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs
+      tabBar={(props) => <CustomTabs {...props} />}
+      screenOptions={{ headerShown: true }}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="statistics" />
+      <Tabs.Screen name="wallet" />
+      <Tabs.Screen name="profile" />
+    </Tabs>
+  );
 };
 
 export default _layout;
