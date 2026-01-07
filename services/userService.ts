5037d0cd61bf89dee358b2f5578601b19758fe37
renesanger <renejsanger@gmail.com>
2026-01-07 13:07:55 -0500

    finished cloudinary CDN

--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,28 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageService";
 
 export const updateUser = async (
   uid: string,
   updatedData: UserDataType
 ): Promise<ResponseType> => {
   try {
+    if (updatedData.image && updatedData?.image?.uri) {
+      const imageUploadRes = await uploadFileToCloudinary(
+        updatedData.image,
+        "users"
+      );
+
+      if (!imageUploadRes.success) {
+        return {
+          success: false,
+          msg: imageUploadRes.msg || "Failed to upload image",
+        };
+      }
+      updatedData.image = imageUploadRes.data;
+    }
+
     const userRef = doc(firestore, "users", uid);
     await updateDoc(userRef, updatedData);
 
