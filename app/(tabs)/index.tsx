b03944f0f56d5a8b7ae33e9b6058f6c42dd4fea7
renesanger <renejsanger@gmail.com>
2026-01-27 16:03:24 -0500

    added HomeCard component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,79 @@
+import HomeCard from "@/components/HomeCard";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
+import { colors, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import { verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
 
 const Home = () => {
-  // const handleLogout = async () => {
-  //   await signOut(auth);
-  // };
+  const { user } = useAuth();
   return (
     <ScreenWrapper>
-      <Typo>Home</Typo>
-      {/* <Button onPress={handleLogout}>
-        <Typo color={colors.black}>Logout</Typo>
-      </Button> */}
+      <View style={styles.container}>
+        {/*Header*/}
+        <View style={styles.header}>
+          <View style={{ gap: 4 }}>
+            <Typo size={16} color={colors.neutral400}>
+              Hello,
+            </Typo>
+            <Typo size={20} fontWeight={"500"}>
+              {user?.name}
+            </Typo>
+          </View>
+          <TouchableOpacity style={styles.searchIcon}>
+            <Icons.MagnifyingGlassIcon
+              size={verticalScale(22)}
+              color={colors.neutral200}
+              weight="bold"
+            />
+          </TouchableOpacity>
+        </View>
+        <ScrollView
+          contentContainerStyle={styles.scrollViewStyle}
+          showsVerticalScrollIndicator={false}
+        >
+          <View>
+            <HomeCard />
+          </View>
+        </ScrollView>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingY._20,
+    marginTop: verticalScale(8),
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    padding: spacingY._10,
+    borderRadius: 50,
+  },
+  floatingButton: {
+    height: verticalScale(50),
+    width: verticalScale(50),
+    borderRadius: 100,
+    position: "absolute",
+    bottom: verticalScale(30),
+    right: verticalScale(30),
+  },
+  scrollViewStyle: {
+    marginTop: spacingY._10,
+    paddingTop: verticalScale(100),
+    gap: spacingY._25,
+  },
+});
---
+++ components/HomeCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Typo from "./Typo";
+const HomeCard = () => {
+  return (
+    <View>
+      <Typo>HomeCard</Typo>
+    </View>
+  );
+};
+
+export default HomeCard;
+
+const styles = StyleSheet.create({});
